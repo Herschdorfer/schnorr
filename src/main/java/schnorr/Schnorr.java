416ebcdda16b1f09b26de80647c60d87c5248913
Thomas Leber <thomas.leber@stud.fh-campuswien.ac.at>
2024-03-15 21:10:20 +0100

    Add SchnorrSignature class and implement sign and verify methods

--- src/main/java/schnorr/Schnorr.java
+++ src/main/java/schnorr/Schnorr.java
@@ -1,7 +1,10 @@
 package schnorr;
 
 import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+
+import crypt_basics.hash.Hash;
 
 public class Schnorr {
 
@@ -17,6 +20,49 @@ public class Schnorr {
         }
     }
 
+    public class SchnorrSignature {
+        public BigInteger r;
+        public BigInteger s;
+
+        public SchnorrSignature(BigInteger r, BigInteger s) {
+            this.r = r;
+            this.s = s;
+        }
+    }
+
+    SchnorrGroup group;
+
+    /**
+     * Private key x and public key y
+     */
+    private BigInteger x;
+    private BigInteger y;
+
+    public Schnorr(int bitLength) {
+
+        group = generateSchnorrGroup(bitLength);
+
+        generateKeyPair();
+
+    }
+
+    /**
+     * Generate a key pair (x, y) such that y = g^x mod p
+     */
+    private void generateKeyPair() {
+        do {
+            x = new BigInteger(group.q.bitLength(), new SecureRandom());
+        } while (x.compareTo(BigInteger.ONE) <= 0 || x.compareTo(group.q) >= 0);
+
+        y = group.g.modPow(x, group.p);
+    }
+
+    /**
+     * Generate a Schnorr group (p, q, g) such that p = qr + 1
+     * 
+     * @param bitLength
+     * @return
+     */
     SchnorrGroup generateSchnorrGroup(int bitLength) {
         BigInteger p;
         BigInteger q;
@@ -44,5 +90,58 @@ public class Schnorr {
         BigInteger g = h.modPow(BigInteger.valueOf(2), p);
 
         return new SchnorrGroup(p, q, g);
+    }
+
+    public SchnorrSignature sign(byte[] message) throws NoSuchAlgorithmException {
+
+        BigInteger k;
+        BigInteger r;
+        BigInteger s;
+
+        if (group.q.bitCount() < 256) {
+            throw new NoSuchAlgorithmException("The bit length of q is less than 256");
+        }
+
+        Hash hash = new Hash(256);
+
+        BigInteger e = new BigInteger(hash.hash(message));
+
+        do {
+            do {
+                k = new BigInteger(group.q.bitLength(), new SecureRandom());
+            } while (k.compareTo(BigInteger.ONE) <= 0 || k.compareTo(group.q) >= 0);
+
+            r = group.g.modPow(k, group.p).mod(group.q);
+        } while (r.equals(BigInteger.ZERO));
+
+        s = k.subtract(x.multiply(e)).mod(group.q);
+
+        return new SchnorrSignature(r, s);
+    }
+
+    public boolean verify(byte[] message, SchnorrSignature signature) throws NoSuchAlgorithmException {
+
+        if (group.q.bitCount() < 256) {
+            throw new NoSuchAlgorithmException("The bit length of q is less than 256");
+        }
+
+        Hash hash = new Hash(256);
+
+        BigInteger e = new BigInteger(hash.hash(message));
+
+        /**
+         * Check if r and s are in the range [1, q - 1]
+         */
+        if (signature.r.compareTo(BigInteger.ZERO) <= 0 || signature.r.compareTo(group.q) >= 0
+                || signature.s.compareTo(BigInteger.ZERO) <= 0 || signature.s.compareTo(group.q) >= 0) {
+            return false;
+        }
+
+        /**
+         * Check if g^s * y^e mod p mod q = r
+         */
+        BigInteger v = group.g.modPow(signature.s, group.p).multiply(y.modPow(e, group.p)).mod(group.p).mod(group.q);
+
+        return v.equals(signature.r);
     }
 }
--- src/test/java/schnorr/SchnorrTest.java
+++ src/test/java/schnorr/SchnorrTest.java
@@ -1,5 +1,6 @@
 package schnorr;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -8,35 +9,48 @@ import java.math.BigInteger;
 
 import org.junit.jupiter.api.Test;
 
+import schnorr.Schnorr.SchnorrSignature;
+
 class SchnorrTest {
 
     @Test
     void testGenerateSchnorrGroup() {
-        Schnorr schnorr = new Schnorr();
-        Schnorr.SchnorrGroup schnorrGroup = schnorr.generateSchnorrGroup(256);
+        Schnorr schnorr = new Schnorr(256);
 
-        assertTrue(schnorrGroup.p.isProbablePrime(100));
-        assertTrue(schnorrGroup.q.isProbablePrime(100));
-        assertEquals(schnorrGroup.q.multiply(BigInteger.valueOf(2)).add(BigInteger.ONE), schnorrGroup.p);
+        assertTrue(schnorr.group.p.isProbablePrime(100));
+        assertTrue(schnorr.group.q.isProbablePrime(100));
+        assertEquals(schnorr.group.q.multiply(BigInteger.valueOf(2)).add(BigInteger.ONE), schnorr.group.p);
     }
 
     @Test
     void testGenerateSchnorrGroupGenerator() {
-        Schnorr schnorr = new Schnorr();
-        Schnorr.SchnorrGroup schnorrGroup = schnorr.generateSchnorrGroup(16);
+        Schnorr schnorr = new Schnorr(16);
 
         /**
          * g^q mod p = 1
          */
-        assertEquals(BigInteger.ONE, schnorrGroup.g.modPow(schnorrGroup.q, schnorrGroup.p));
+        assertEquals(BigInteger.ONE, schnorr.group.g.modPow(schnorr.group.q, schnorr.group.p));
 
         /**
          * g^i mod p != 1 for 1 < i < q
          */
-        for (int i = 1; i < schnorrGroup.q.intValue(); i++) {
-            BigInteger g = schnorrGroup.g.modPow(BigInteger.valueOf(i), schnorrGroup.p);
+        for (int i = 1; i < schnorr.group.q.intValue(); i++) {
+            BigInteger g = schnorr.group.g.modPow(BigInteger.valueOf(i), schnorr.group.p);
             assertNotEquals(BigInteger.ONE, g);
         }
+
+    }
+
+    @Test
+    void testVerify() {
+
+        Schnorr schnorr = new Schnorr(1024);
+
+        assertDoesNotThrow(() -> {
+            SchnorrSignature signature = schnorr.sign("Hello, World!".getBytes());
+
+            assertTrue(schnorr.verify("Hello, World!".getBytes(), signature));
+        });
 
     }
 }
